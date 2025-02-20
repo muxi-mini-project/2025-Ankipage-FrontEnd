dc778b27e5e17bf23f29d40b4feff892399ffaff
bingling-sama <bingling_sama@qq.com>
2025-02-20 18:54:14 +0800

    ✨ feat: integrate messaging functionality with background ping response

--- popup.tsx
+++ popup.tsx
@@ -1,23 +1,17 @@
-import { useState } from "react"
-
 import "./style.css"
 
+import { sendToBackground } from "@plasmohq/messaging/dist"
+
 function IndexPopup() {
-  const [data, setData] = useState("")
+  const sendMessage = () => {
+    sendToBackground({ name: "ping" }).then((res) => {
+      alert(res.message)
+    })
+  }
 
   return (
     <div className="p-4">
-      <h2>
-        Welcome to your{" "}
-        <a href="https://www.plasmo.com" target="_blank" rel="noreferrer">
-          Plasmo
-        </a>{" "}
-        Extension!
-      </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank" rel="noreferrer">
-        View Docs
-      </a>
+      <button onClick={sendMessage}>Send Message</button>
     </div>
   )
 }
