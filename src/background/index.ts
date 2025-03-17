fef3406a765221721559f606084e4999c8d3e022
bingling-sama <bingling_sama@qq.com>
2025-03-17 16:28:49 +0800

    ✨ feat: add side panel support and enable automatic opening on extension icon click

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,3 +1,12 @@
 import { checkLoginState } from "~utils"
 
+// Enable side panel and set it to open on extension icon click
+chrome.runtime.onInstalled.addListener(() => {
+  // Set default side panel behavior
+  chrome.sidePanel.setPanelBehavior({
+    openPanelOnActionClick: true
+  })
+})
+
+// Check login state
 checkLoginState()
